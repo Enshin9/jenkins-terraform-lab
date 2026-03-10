a8e4a17a707fca54d78a3626ab029abfbd1be2a9
Enshin9 <131129832+Enshin9@users.noreply.github.com>
2026-03-10 21:03:30 +0900

    vpc.tf を更新
    ネットワークの作成

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,33 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = var.routetable_name
+  }
+}
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}

59fc29357f3339b296be1f47c4f7701b7b2ccb6d
Enshin9 <131129832+Enshin9@users.noreply.github.com>
2026-03-10 20:06:17 +0900

    variables.tf
    Variables

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr" {
+   description = "vpc cidr"
+   type = string 
+}
+variable "vpc_name" {
+   description = "vpc name tag"
+   type = string 
+}
+variable "subnet_cidr" {
+   description = "subnet cidr"
+   type = string 
+}
+variable "subnet_name" {
+   description = "subnet name tag"
+   type = string 
+}
+variable "ec2_name" {
+   description = "ec2 name tag"
+   type = string 
+}

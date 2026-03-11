38edc37e1e5f8e23b55d5924082d50c50bfa64a4
Enshin9 <131129832+Enshin9@users.noreply.github.com>
2026-03-11 09:46:10 +0900

    sg.tf を更新

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_443" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -18,10 +18,17 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_443" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_22" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main._cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
-  ip_protocol       = "ssh"
+  ip_protocol       = "tcp"
   to_port           = 22
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_8080" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {

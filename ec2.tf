109e2d1478cf7cf366c5006d27620ff5e4f54812
Enshin9 <131129832+Enshin9@users.noreply.github.com>
2026-03-11 20:15:23 +0900

    ec2.tf を更新

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "my-ec2" {
   vpc_id = aws_vpc.main.id
   subnet_id = awd_subnet.main.id #指定したパブリックサブネットに配置
   associate_public_ip_address = true # インターネットに公開するためのパブリック IP を付与
-  security_groups = aws_security_group.main.id # 指定したセキュリティグループを適用
+  security_groups = aws_security_group.allow_tls.id # 指定したセキュリティグループを適用
   tags = {
     Name = var.ec2_name
   }

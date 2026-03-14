ed341fe50e6bc27fda59c73efbe45266feb4f06c
wwx <sadozoe@gmail.com>
2026-03-14 14:41:06 +0900

    user_data追加

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,19 @@ resource "aws_instance" "my-ec2" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id] # 指定したセキュリティグループを適用
   availability_zone = "ap-northeast-1a"
   key_name = "jenkins_test"
+## Docker Jenkins install
+  user_data = <<-EOF
+#!/bin/bash
+apt update -y
+apt install -y docker.io
+
+systemctl start docker
+systemctl enable docker
+
+docker run -d -p 8080:8080 jenkins/jenkins:lts
+EOF
+
+
   tags = {
     Name = var.ec2_name
   }

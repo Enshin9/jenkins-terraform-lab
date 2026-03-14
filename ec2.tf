20641db970cce94f96eb0a73b6217759564429e8
wwx <sadozoe@gmail.com>
2026-03-14 13:31:06 +0900

    ec2の修正 keypair設定追加

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "my-ec2" {
   ami = var.ami_id
   instance_type = "t2.micro"
-  vpc_id = aws_vpc.main.id
-  subnet_id = awd_subnet.main.id #指定したパブリックサブネットに配置
+  subnet_id = aws_subnet.main.id #指定したパブリックサブネットに配置
   associate_public_ip_address = true # インターネットに公開するためのパブリック IP を付与
-  security_groups = aws_security_group.allow_tls.id # 指定したセキュリティグループを適用
+  vpc_security_group_ids = [aws_security_group.allow_tls.id] # 指定したセキュリティグループを適用
+  availability_zone = "ap-northeast-1a"
+  key_name = "jenkins_test"
   tags = {
     Name = var.ec2_name
   }
